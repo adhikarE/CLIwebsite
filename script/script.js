d05e2717cbdbf29f05f9d72116ee5729d087c681
8rupees <122161229+8rupees@users.noreply.github.com>
2024-05-09 21:17:34 +0530

    Update script.js
    Added a surprise ;)

--- script/script.js
+++ script/script.js
@@ -5,7 +5,7 @@ function compile() {
     var output = "";
     var userText = document.getElementById("user");
 
-    outputText.innerHTML += "<br>" + "visitor@ubuntu$" + input + "<br>";
+    outputText.innerHTML += "<br>" + "[visitor@ubuntu ~]$" + input + "<br>";
 
     if (input === "") {} else{
     
@@ -13,17 +13,17 @@ function compile() {
             case "help":
                 output = "List of available commands:-<br>"
                 output += "- <b>whoami</b>:&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;Tell \"Who am i\".<br>"
-                output += "- <b>exp</b>:&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;Lists my work experience till date.<br>"
-                output += "- <b>socials</b>:&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;Shows contact information.<br>"
-                output += "- <b>help</b>:&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;Lists down the valid commands.<br>"
-                output += "- <b>clear</b>:&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;Clears the terminal.";
+                output += "- <b>exp</b>:&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;Lists my work experience till date.<br>"
+                output += "- <b>socials</b>:&#160;&#160;&#160;&#160;&#160;&#160;&#160;Shows contact information.<br>"
+                output += "- <b>help</b>:&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;Lists down the valid commands.<br>"
+                output += "- <b>clear</b>:&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;Clears the terminal.";
                 break;
             case "whoami":
                 output += "Namaste!<br>I am a Network/Security enthusiast & a student who currently completing my B. Tech in Computer Science and Engineering in with a specialization in Cyber Security.<br>I'm currently continuing my studies at NMIMS, Mumbai.<br>For more details/updates please check my socials!";
                 break;
             case "exp":
                 output += "<b>Sapio Analytics Mumbai</b>: &#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;<i>Data Privacy Auditor</i><br><br>"
-                output += "<b>IT-11 Computer Education Center</b>: <i>Programming Instructor</i>."
+                output += "<b>IT-11 Computer Education Center</b>: &#160;&#160;&#160;&#160;&#160;&#160;&#160;&#160;<i>Programming Instructor</i>."
                 break;
             case "socials":
                 output += "<a target=\"_blank\" href=\"https://linkedin.com/in/8rupees\" style=\"color: darkgreen;\" >LinkedIn</a><br>";
@@ -33,6 +33,16 @@ function compile() {
                 break;
             case "clear":
                 outputText.innerHTML = "";
+                break;
+            case "ls":
+                output += "README.txt";
+                break;
+            case "cat readme.txt":
+                output += "Fuzzing around i see... You might aswell expect \"cat etc/passwd\" to work. IT'S NOT A REAL TERMINAL!";
+                break;
+            case "cat etc/passwd":
+                window.location = "https://youtu.be/dQw4w9WgXcQ?si=1WLt2cSa__GScU-O";
+                output += "SEE YA!";
                 break;
             default:
                 output = "Command not found: " + input;
@@ -45,7 +55,7 @@ function compile() {
     
         outputText.scrollTop = outputText.scrollHeight;
     
-        userText.textContent = "visitor@ubuntu$";
+        userText.textContent = "[visitor@ubuntu ~]$";
     }
 
     document.getElementById("line").addEventListener("keypress", function(event){
